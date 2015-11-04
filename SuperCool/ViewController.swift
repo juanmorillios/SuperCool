2149f16350e96551834a9d34ffab844d3aab1948
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-11-04 15:26:20 +0100

    CourseiOS9andSwift2 - FirstGame
    First Game  of Series made from iOS course 9 andSwift2

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLogoImage: UIImageView!
+    @IBOutlet weak var coolBgImage: UIImageView!
+    @IBOutlet weak var unCoolBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeSuperCoolBtn(sender: AnyObject) {
+        
+        
+        //Ocultamos logo y fondo de la App
+        coolLogoImage.hidden = false
+        coolBgImage.hidden = false
+        unCoolBtn.hidden = true
+        
+        
+        
+        
+    }
 
 }
 
